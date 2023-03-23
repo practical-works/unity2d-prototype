738e99f3b3716d684be5ca51f60307465178ccbb
Ambratolm <Ambratolm@gmail.com>
2023-03-23 17:33:30 +0000

    Added some stuff xD

--- Assets/Scripts/Managers/Singleton.cs
+++ Assets/Scripts/Managers/Singleton.cs
@@ -5,21 +5,28 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
     private static T s_instance;
 
+    [ShowInInspector]
     public static T Instance
     {
         get
         {
-            if (!s_instance) s_instance = FindObjectOfType<T>();
-            if (!s_instance) s_instance = new GameObject(typeof(T).Name).AddComponent<T>();
+            if (!s_instance)
+            {
+                s_instance = FindObjectOfType<T>();
+                if (s_instance) s_instance.name = $"${s_instance.name}";
+                else s_instance = new GameObject($"${typeof(T).Name}").AddComponent<T>();
+            }
             return s_instance;
         }
     }
 
-    internal virtual void Awake() => PreventMultipleInstances();
+    //[ShowInInspector] private bool IsStagedPrefab => !!PrefabStageUtility.GetPrefabStage(gameObject);
 
-    internal virtual void OnValidate() => PreventMultipleInstances();
+    internal virtual void Awake() => PreventMultipleInstancesRuntime();
 
-    private void PreventMultipleInstances()
+    internal virtual void OnValidate() => PreventMultipleInstancesRuntime();
+
+    private void PreventMultipleInstancesRuntime()
     {
         // Todo Fix: Object prefab + in-scene thinks it got 2 instances by that & triggers the destruction
         if (s_instance && s_instance != this)
@@ -27,9 +34,14 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
             if (Application.isPlaying) Destroy(this);
             else UnityEditor.EditorApplication.delayCall += () => DestroyImmediate(this);
             Debug.LogWarning($"Cannot create multiple instances of {s_instance.GetType()} component.\nAn instance is already attached to " +
-                $"\"{s_instance.name}\" object. Therefore, created duplicate instance attached to \"{gameObject.name}\" object is destroyed.");
+                $"\"{s_instance.name}\" object. Therefore, duplicate instance attached to \"{gameObject.name}\" object is destroyed.");
         }
         else if (!s_instance)
             s_instance = this as T;
     }
+
+    //private void PreventMultipleInstancesEditor()
+    //{
+    //    PreventMultipleInstancesRuntime();
+    //}
 }
